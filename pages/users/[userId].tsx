d8c1401f899d1908211b1dc7bc31b0318515f9b6
İsmail Daşcı <109577378+ismaildasci@users.noreply.github.com>
2024-01-06 22:06:46 +0300

    Load & Create Posts

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,11 +1,12 @@
 import Header from "@/components/Header";
+import PostFeed from "@/components/posts/PostFeed";
 import UserBio from "@/components/users/UserBio";
 import UserHero from "@/components/users/userHero";
 import useUser from "@/hooks/useUser";
 import { useRouter } from "next/router";
 import { ClipLoader } from "react-spinners";
 
-const UserView = () => {
+const UserView = (props) => {
     const router = useRouter();
     const { userId } = router.query;
 const {data: fetchedUser,isLoading} = useUser(userId as string);
@@ -28,6 +29,7 @@ return (
    <Header showBackArrow  label={fetchedUser?.name} />
    <UserHero userId={userId as string} />
    <UserBio userId={userId as string} />
+   <PostFeed userId={userId as string} />
    </>
     );
 }
