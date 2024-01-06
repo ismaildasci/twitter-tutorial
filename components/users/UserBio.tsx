506933c5f30b629c4eb10fe79be94be4d6a27789
İsmail Daşcı <109577378+ismaildasci@users.noreply.github.com>
2024-01-06 23:37:07 +0300

    Like & Follow Functionality

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -5,6 +5,7 @@ import { useMemo } from 'react';
 import Button from '../Button';
 import { BiCalendar } from 'react-icons/bi';
 import useEditModal from '@/hooks/useEditModal';
+import useFollow from '@/hooks/useFollow';
 
 interface UserBioProps {
     userId: string;
@@ -13,6 +14,8 @@ const UserBio: React.FC<UserBioProps> = ({userId}) => {
     const {data: currentUser} = useCurrentUser();
     const {data: fetchedUser} = useUser(userId);
     const editModal = useEditModal();
+
+    const {isFollowing, toggleFollow} = useFollow(userId);
 
     const createdAt = useMemo(() => {
         if (!fetchedUser?.createdAt) {
@@ -29,9 +32,10 @@ const UserBio: React.FC<UserBioProps> = ({userId}) => {
                 <Button secondary label="Edit" onClick={editModal.onOpen} />
             ) : (
               <Button
-              onClick={() => {}}
-              label="Follow"
-              secondary
+              onClick={toggleFollow}
+              label={isFollowing ? 'Unfollow' : 'Follow'}
+              secondary={!isFollowing}
+              outline={isFollowing}
               />
             )}
             </div>
